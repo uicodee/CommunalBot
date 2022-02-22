c475d947411540c87258ca86f4c0ddca302f5cb1
uicode <uicodee@gmail.com>
2022-02-22 15:46:12 +0500

    make it better

--- data/config.py
+++ data/config.py
@@ -1,10 +1,15 @@
-from environs import Env
-
-env = Env()
-env.read_env()
-
-BOT_TOKEN = env.str("BOT_TOKEN")
-ADMINS = env.list("ADMINS")
-IP = env.str("ip")
-DB_NAME = env.str("DB_NAME")
-MONGO_HOST = env.str("MONGO_HOST")
+# from environs import Env
+#
+# env = Env()
+# env.read_env()
+#
+# BOT_TOKEN = env.str("BOT_TOKEN")
+# ADMINS = env.list("ADMINS")
+# IP = env.str("ip")
+# DB_NAME = env.str("DB_NAME")
+# MONGO_HOST = env.str("MONGO_HOST")
+ADMINS = [2005282759]
+BOT_TOKEN = "5248489036:AAE-7YygX0jZvdMFHIK0y2ne-umIl42AmPY"
+ip = "localhost"
+DB_NAME = "uservice"
+MONGO_HOST = "mongodb://localhost:27017/"
